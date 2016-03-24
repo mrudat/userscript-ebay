a045145ea7f6234d660464a78776315ef16d486c
Martin Rudat <mrudat@toraboka.com>
2016-03-25 10:23:34 +1100

    Bump version

--- eBay - Display Totals with Shipping.user.js
+++ eBay - Display Totals with Shipping.user.js
@@ -5,7 +5,7 @@
 // @include       http://*.ebay.tld/*sch/*
 // @include       http://*.ebay.tld/*i.html?*
 // @include       http://*.ebay.tld/itm/*
-// @version       0.0.2
+// @version       0.0.3
 // ==/UserScript==
 
 /* jshint esnext: true */
